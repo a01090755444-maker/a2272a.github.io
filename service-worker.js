116e6dcb501cb9eee975b44db18992492570ddde
a01090755444-maker <a01090755444@gmail.com>
2025-12-15 18:14:11 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,15 @@
-const CACHE_NAME = "phone-special-cache-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+// 캐시 안 쓰고 항상 네트워크에서 가져오기
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
